0b7ffd6c0be99b4f8fcbc689ec8dcbca30495343
Bob-Hinkley <bob.hinkley1@gmail.com>
2017-01-16 08:25:45 -0600

    Got the Prev/Next buttons successfully appending the Dom.

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
       success: function(data){
         // console.log(data);
 
-        data.forEach(function(upsilon) {
+        // data.forEach(function(upsilon) {
 
-        });//forEach function
+        // });//forEach function
 
           appendDom(data[currentStudent]);
 
@@ -22,39 +22,42 @@ $(document).ready(function(){
             } else {
               currentStudent++;
             }
-            changeStudent(data, currentStudent);
-            console.log(currentStudent);
+            changeStudent(data);
+            // console.log(currentStudent);
           });
 
           //Prev button
           $('#prev').on('click', function () {
             if (currentStudent == 0) {
-              currentStudent = data.length-1;
+              currentStudent = data.length - 1;
             } else {
               currentStudent--;
             }
-            changeStudent(data, currentStudent);
-            console.log(currentStudent);
+            changeStudent(data);
+            // console.log(currentStudent);
           });
+
+
+                //Appends dom with student information
+                function appendDom(upsilon) {
+                  var $upsilonDiv = $('<div class="upsilon"></div>');
+
+                  // $upsilonDiv.append('img')
+                  $upsilonDiv.append('<h2>'+ upsilon.name +'</h>')
+                  $upsilonDiv.append('<p>'+ upsilon.githubUserName +'</p>')
+                  $upsilonDiv.append('<p>'+ upsilon.shoutout +'</p>')
+
+                  $('.ajax-stuff').append($upsilonDiv);
+                };
+
+                //Removes old student div and switches to current student div
+                function changeStudent (data) {
+                    $('.ajax-stuff').find('div').remove();
+                    var newStudent = data[currentStudent];
+                    appendDom(newStudent);
+                    console.log(newStudent);
+                };
 
       }//Success Function
     });//AJAX
   }); //Document Ready function
-
-  //Removes old student div and switches to current student div
-  function changeStudent (data, currentStudent) {
-      $('.ajax-stuff').closest('div').remove();
-      appendDom(data[currentStudent]);
-  };
-
-  //Appends dom with student information
-  function appendDom(upsilon) {
-    var $upsilonDiv = $('<div class="upsilon"></div>');
-
-    // $upsilonDiv.append('img')
-    $upsilonDiv.append('<h2>'+ upsilon.name +'</h>')
-    $upsilonDiv.append('<p>'+ upsilon.githubUserName +'</p>')
-    $upsilonDiv.append('<p>'+ upsilon.shoutout +'</p>')
-
-    $('.ajax-stuff').append($upsilonDiv);
-  };

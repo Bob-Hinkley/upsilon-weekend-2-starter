39dc689316b5b00a266dd33eacd2685aa19b9158
Bob-Hinkley <bob.hinkley1@gmail.com>
2017-01-16 09:24:48 -0600

    Added minor styling.  Still working on getting an gui index at the bottom of the screen like in the xample.

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -2,62 +2,56 @@ var currentStudent = 0;
 
 $(document).ready(function(){
 
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(data){
-        // console.log(data);
+  $.ajax({
+    type: "GET",
+    url: "/data",
+    success: function(data){
+      // console.log(data);
 
-        // data.forEach(function(upsilon) {
+      // data.forEach(function(upsilon) {
+      // });//forEach function
+      appendDom(data[currentStudent]);
 
-        // });//forEach function
+      //Next button
+      $('#next').on('click', function () {
+        if (currentStudent == data.length - 1) {
+          currentStudent = 0;
+        } else {
+          currentStudent++;
+        }
+        changeStudent(data);
+        // console.log(currentStudent);
+      });
 
-          appendDom(data[currentStudent]);
+      //Prev button
+      $('#prev').on('click', function () {
+        if (currentStudent == 0) {
+          currentStudent = data.length - 1;
+        } else {
+          currentStudent--;
+        }
+        changeStudent(data);
+        // console.log(currentStudent);
+      });
 
+      //Appends dom with student information
+      function appendDom(upsilon) {
+        var $upsilonDiv = $('<div class="upsilon"></div>');
 
-          //Next button
-          $('#next').on('click', function () {
-            if (currentStudent == data.length - 1) {
-              currentStudent = 0;
-            } else {
-              currentStudent++;
-            }
-            changeStudent(data);
-            // console.log(currentStudent);
-          });
+        $upsilonDiv.append('<h2>'+ upsilon.name +'</h>')
+        $upsilonDiv.append('<p>Github:  '+ upsilon.githubUserName +'</p>')
+        $upsilonDiv.append('<p>'+ upsilon.shoutout +'</p>')
 
-          //Prev button
-          $('#prev').on('click', function () {
-            if (currentStudent == 0) {
-              currentStudent = data.length - 1;
-            } else {
-              currentStudent--;
-            }
-            changeStudent(data);
-            // console.log(currentStudent);
-          });
+        $('.ajax-stuff').append($upsilonDiv);
+      };
 
-
-                //Appends dom with student information
-                function appendDom(upsilon) {
-                  var $upsilonDiv = $('<div class="upsilon"></div>');
-
-                  // $upsilonDiv.append('img')
-                  $upsilonDiv.append('<h2>'+ upsilon.name +'</h>')
-                  $upsilonDiv.append('<p>'+ upsilon.githubUserName +'</p>')
-                  $upsilonDiv.append('<p>'+ upsilon.shoutout +'</p>')
-
-                  $('.ajax-stuff').append($upsilonDiv);
-                };
-
-                //Removes old student div and switches to current student div
-                function changeStudent (data) {
-                    $('.ajax-stuff').find('div').remove();
-                    var newStudent = data[currentStudent];
-                    appendDom(newStudent);
-                    console.log(newStudent);
-                };
-
-      }//Success Function
-    });//AJAX
-  }); //Document Ready function
+      //Removes old student div and switches to current student div
+      function changeStudent (data) {
+        $('.ajax-stuff').find('div').remove();
+        var newStudent = data[currentStudent];
+        appendDom(newStudent);
+        console.log(newStudent);
+      };    
+    }//Success Function
+  });//AJAX
+}); //Document Ready function
